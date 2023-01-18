056609366bd774f5e5f3eaf270b80b39e005eb6f
gattihub <flavi503@hotmail.com>
2023-01-18 00:32:40 -0300

    feat: addToCart JS and CSS, navbar border:hover

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const adicionar = document.querySelector(".btn-sum");
 const diminuir = document.querySelector(".btn-sub");
 const quantidade = document.querySelector(".btn-qtd");
+const cart = document.querySelector(".counter");
+const buttonCart = document.querySelector(".button-cart");
 
 function somar() {
 
@@ -12,9 +14,19 @@ function somar() {
 function subtrair() {
     if (quantidade.value > 0) {
         quantidade.innerHTML = `${--quantidade.value}`;
-        console.log(quantidade)
+        
     }
 }
 
 adicionar.onmousedown = somar;
 diminuir.onmouseup = subtrair;
+
+function addToCart() {
+    if (quantidade.value > 0) {
+    cart.classList.add("showCounter");
+    cart.innerHTML = `${quantidade.value}`;
+} 
+}
+
+buttonCart.onmouseup = addToCart;
+
